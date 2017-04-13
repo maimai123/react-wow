d81b166c6f79cbd333e453000807f9a54fa995a3
zhaozhuoboy <zhaozhuoboy@163.com>
2017-04-14 07:49:38 +0800

    a

--- src/App.js
+++ src/App.js
@@ -5,12 +5,84 @@ class App extends React.Component {
 
   render () {
     return(
-      <div style={{width:"80%",margin:"0 auto", display:"flex", justifyContent: "space-between"}}>
+      <div>
+        <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
         <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
         <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
         <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
         <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
         <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
+      <div style={{width:"80%",margin:"50px auto", display:"flex", justifyContent: "space-between"}}>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.4s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s"></section>
+        <section className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.7s"></section>
+      </div>
       </div>
     )
   }
